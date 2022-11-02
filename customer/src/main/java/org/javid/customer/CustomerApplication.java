cbbd6d32e59a355c741f748d1c022b3d2a40011e
Javid Rafiei <javid.igap@gmail.com>
2022-11-03 00:04:36 +0330

    Enabling FeignClients in customer microservice.

--- customer/src/main/java/org/javid/customer/CustomerApplication.java
+++ customer/src/main/java/org/javid/customer/CustomerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigurationProperties({Properties.class})
+@EnableFeignClients(basePackages = "org.javid.clients")
 public class CustomerApplication {
 
     public static void main(String[] args) {
