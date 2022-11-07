d7df16fb1c4374fa8e32de61e8b68cbbffdae241
Javid Rafiei <javid.igap@gmail.com>
2022-11-07 11:28:05 +0330

    Enabling RabbitMQL in notification microservice.

--- notification/src/main/java/org/javid/notification/NotificationApplication.java
+++ notification/src/main/java/org/javid/notification/NotificationApplication.java
@@ -4,11 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.javid.notification",
+                "org.javid.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+/* for seeing uncomment this block
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig config) {
+        return args -> producer.publish(
+                new Person("Javid", 35),
+                config.getInternalExchange(),
+                config.getInternalNotificationRoutingKey());
+    }
+
+    record Person(String name, int age) {
+    }
+*/
 }
